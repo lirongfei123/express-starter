42e3eb6dc6d45574a2d2f12d2487ed4966f9adb0
rongping.lrp <rongping.lrp@alibaba-inc.com>
2017-05-10 11:40:37 +0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+var methodOverride = require('method-override');
+var serveStatic = require('serve-static')
+var config = require('config');
+var middleware = require('./middleware');
+var router = require('./router');
 var app = express();
+var port = config.get('express.port');
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.text({ type: 'text/html' }))
+app.use(cookieSession({
+    name: 'session',
+    keys: ['demo@qq.com'],
+    maxAge: 24 * 60 * 60 * 1000
+}));
+app.use(methodOverride());
+middleware(app);
+router(app);
+app.listen(port || 3001, function() {
+    console.log('server is starting');
+});
